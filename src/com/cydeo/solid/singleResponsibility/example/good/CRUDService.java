94bb41feeef0b4c6ddfe500c92c96d2d89a13560
Ekaterina <ekaterina.sementeva@gmail.com>
2022-08-30 12:43:00 -0400

    Crud

--- src/com/cydeo/solid/singleResponsibility/example/good/CRUDService.java
+++ src/com/cydeo/solid/singleResponsibility/example/good/CRUDService.java
@@ -7,4 +7,17 @@ public class CRUDService {
         //create a user
         //Send it to Data Base related part
     }
+
+    public static void read(){
+        //read a user
+        //Send it to Data Base related part
+    }
+    public static void update(){
+        //update a user
+        //Send it to Data Base related part
+    }
+    public static void delete(){
+        //delete a user
+        //Send it to Data Base related part
+    }
 }
