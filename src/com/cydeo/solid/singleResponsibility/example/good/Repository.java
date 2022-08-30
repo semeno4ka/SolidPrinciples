af0f70b5fc5e1400fc10c2de32b4d1deb7c1c44f
Ekaterina <ekaterina.sementeva@gmail.com>
2022-08-30 12:45:28 -0400

    DB related part

---
+++ src/com/cydeo/solid/singleResponsibility/example/good/Repository.java
@@ -0,0 +1,24 @@
+package com.cydeo.solid.singleResponsibility.example.good;
+
+public class Repository {
+    //database related actions
+
+    public static void create(){
+      // Connect to DB
+        //Create User in DB
+}
+
+    public static void read(){
+        // Connect to DB
+        //Read User in DB
+    }
+    public static void update(){
+        // Connect to DB
+        //Update User in DB
+    }
+    public static void delete(){
+        // Connect to DB
+        //Delete User in DB
+    }
+
+}
